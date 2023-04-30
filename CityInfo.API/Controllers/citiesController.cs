190498c138c3379e6d95be153cdc7b29dcc71e85
Fredricc <kfred45.fk@gmail.com>
2023-04-30 20:01:51 +0300

    Demo.Inspecting the ASP.NET Core API Project Code Structure

---
+++ CityInfo.API/Controllers/citiesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class citiesController : ControllerBase
+    {
+        //[ApiController]
+        [Route("[controller]")]
+        //public IActionResult Index()
+        {
+            //return View();
+        }
+    }
+}
