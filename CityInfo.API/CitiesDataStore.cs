72b6c049c8fe1fe1ddf56da44f395400c9a59968
Fredricc <kfred45.fk@gmail.com>
2023-05-02 03:22:24 +0300

    Demo.Improving the Architecture with Model Classes

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -1,6 +1,42 @@
-﻿namespace CityInfo.API
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
 {
     public class CitiesDataStore
     {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The city with the cathedral that was never really finished."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Thika",
+                    Description = "Kiambu county first city."
+                },
+                new CityDto()
+                {
+                    Id = 4,
+                    Name = "Nairobi",
+                    Description = "The City with the best roads in East Africa."
+                }
+            };
+        }
     }
 }
---
+++ CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public int NumberOfPointsOfInterest { get; set; }
+
+        //public ICollection<PointOfInterestDto> pointsOfInterest();
+    }
+}
