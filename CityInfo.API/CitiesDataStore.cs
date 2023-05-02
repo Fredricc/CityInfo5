4c38de2f91e8c6b4dc186181a9a266be2f80acb6
Fredricc <kfred45.fk@gmail.com>
2023-05-02 17:16:41 +0300

    Demo.Returning Child Resources

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -16,27 +16,87 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto ()
+                        {
+                            Id = 1,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the middle of Manhattan."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        },
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
-                    Description = "The city with the cathedral that was never really finished."
+                    Description = "The city with the cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the middle of Manhattan."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        },
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Thika",
-                    Description = "Kiambu county first city."
+                    Description = "Kiambu county first city.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the middle of Manhattan."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 4,
                     Name = "Nairobi",
-                    Description = "The City with the best roads in East Africa."
+                    Description = "The City with the best roads in East Africa.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 7,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the middle of Manhattan."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 8,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        }
+
                 }
-            };
+            } };
         }
     }
 }
--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -9,8 +9,15 @@ namespace CityInfo.API.Models
         public string Name { get; set; } = string.Empty;
         [MaxLength(200)]
         public string? Description { get; set; }
-        public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest 
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+         }
 
-        //public ICollection<PointOfInterestDto> pointsOfInterest();
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+                = new List<PointOfInterestDto>();
     }
 }
