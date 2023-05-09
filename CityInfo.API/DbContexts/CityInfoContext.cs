802bb1205b1fe7c3082c53152838dbf9b94246ec
Fredricc <kfred45.fk@gmail.com>
2023-05-09 15:21:52 +0300

    Demo.Seeding the Database with Data

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -13,5 +13,83 @@ namespace CityInfo.API.DbContexts
         {
             
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>()
+                 .HasData(
+                new City("New York City")
+                {
+                    Id = 1,
+                    Description = "The one with that big park."
+                },
+                new City("Antwerp")
+                {
+                    Id = 2,
+                    Description = "The one with the Cathedral that was never really finished."
+                },
+                new City("Thika")
+                {
+                    Id = 3,
+                    Description = "Kiambu County first city with an industrial park."
+                },
+                new City("Kisumu City")
+                {
+                    Id = 4,
+                    Description = "The City at the lake side."
+                }
+                );
+            modelBuilder.Entity<PointOfInterest>()
+                .HasData(
+                new PointOfInterest("Central Park")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "The most visited urban park in the united states"
+                },
+                new PointOfInterest("Empire State Building")
+                {
+                    Id = 2,
+                    CityId = 1,
+                    Description = "A 102 - Story skyscraper located in midtown ManHattan"
+                },
+                 new PointOfInterest("Central Park")
+                 {
+                     Id = 3,
+                     CityId = 2,
+                     Description = "The most visited urban park in the united states"
+                 },
+                new PointOfInterest("Cathedral")
+                {
+                    Id = 4,
+                    CityId = 2,
+                    Description = "A 102 - Story skyscraper located in midtown ManHattan"
+                },
+                 new PointOfInterest("DelMontty")
+                 {
+                     Id = 5,
+                     CityId = 3,
+                     Description = "The most visited urban park in the united states"
+                 },
+                new PointOfInterest("Thika Road")
+                {
+                    Id = 6,
+                    CityId = 3,
+                    Description = "A 102 - Story skyscraper located in midtown ManHattan"
+                },
+                 new PointOfInterest("River Nile")
+                 {
+                     Id = 7,
+                     CityId = 4,
+                     Description = "The most visited urban park in the united states"
+                 },
+                new PointOfInterest("Lake Victoria")
+                {
+                    Id = 8,
+                    CityId = 4,
+                    Description = "A 102 - Story skyscraper located in midtown ManHattan"
+                });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CityInfo.API/Migrations/20230509121733_DataSeed.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "The one with that big park.", "New York City" },
+                    { 2, "The one with the Cathedral that was never really finished.", "Antwerp" },
+                    { 3, "Kiambu County first city with an industrial park.", "Thika" },
+                    { 4, "The City at the lake side.", "Kisumu City" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The most visited urban park in the united states", "Central Park" },
+                    { 2, 1, "A 102 - Story skyscraper located in midtown ManHattan", "Empire State Building" },
+                    { 3, 2, "The most visited urban park in the united states", "Central Park" },
+                    { 4, 2, "A 102 - Story skyscraper located in midtown ManHattan", "Cathedral" },
+                    { 5, 3, "The most visited urban park in the united states", "DelMontty" },
+                    { 6, 3, "A 102 - Story skyscraper located in midtown ManHattan", "Thika Road" },
+                    { 7, 4, "The most visited urban park in the united states", "River Nile" },
+                    { 8, 4, "A 102 - Story skyscraper located in midtown ManHattan", "Lake Victoria" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
