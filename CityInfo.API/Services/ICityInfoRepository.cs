774930247e35c8f4fb5573f9644ad63083748a1a
Fredricc <kfred45.fk@gmail.com>
2023-05-10 03:30:16 +0300

    Demo.Introducing the Repository Pattern Part 2

---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, 
+            int pointOfInterestId);
+    }
+}
