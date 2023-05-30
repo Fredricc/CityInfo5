f9bde1a232d9305303bafd94eb1e420a6afd5beb
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-05-30 12:10:19 +0300

    Demo.Filtering Resources

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -5,7 +5,8 @@ namespace CityInfo.API.Services
 {
     public interface ICityInfoRepository
     {
-        Task<IEnumerable<City>> GetCitiesAsync(string? Name);
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<IEnumerable<City>> GetCitiesAsync(string? name);
         Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
         Task<bool> CityExistsAsync(int cityId);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
